b4b32677a65d889db204703bb19c3b0be4b083a5
Arun Bangay <arun.bangay@challoners.org>
2023-12-01 14:27:54 +0000

    change of variables

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 file = (((open('puzzleInput.txt', 'r')).read()).strip('\n')).splitlines()
-total = 0
-print(file)
 
 for i in range(len(file)):
-    for k in range(len)
-    print(k)
-    for j in range(len(file[i])):
-        if file[i][j].isnumeric() == True:
-            True
+    keyElement = []
+    keyNumbers = []
+    for k in range(len(file[i])):
+        keyElement.append((file[i][k]))
+    found1 = False
+    for m in range(len(keyElement)):
+        while found1 == False:
+            if keyElement[m].isnumeric() == True:
+                keyNumbers.append(keyNumbers[m])
+                found1 = True
+    reversed = keyElement.reverse()
+    found2 = False
+    for n in range(len(reversed)):
+        while found2 == False:
+            if reversed[n].isnumeric() == True: 
+                keyNumbers.append(reversed[n])
+                found2 = True
 
-print(total)
+print(reversed)
